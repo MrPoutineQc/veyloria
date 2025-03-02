d2f15bbc2fdb4c15951e2022b97dbc9639f25dff
VipexV4B <jeremytheriault23@gmail.com>
2025-03-01 21:55:37 -0500

    partie Flaure

--- script.js
+++ script.js
@@ -3,10 +3,18 @@ document.addEventListener("DOMContentLoaded", () => {
     let rightArrow = document.getElementById("rightArrow");
     let faune = document.getElementById("faune");
     let flaure = document.getElementById("flaure");
-    let habitants = document.getElementById("habitants")
-    let planet = document.getElementById("planetImg")
+    let habitants = document.getElementById("habitants");
+    let planet = document.getElementById("planetImg");
     let Habitants = document.getElementById("Habitants");
+    let partieFlaure = document.getElementById("partieFlaure");
+    let partieFaune = document.getElementById("partieFaune")
     let state = "Habitants";
+
+    if (state === "Habitants") {
+        partieFaune.style.display = "none"
+        partieFlaure.style.display = "none"
+    }
+
 
     leftArrow.addEventListener("click", () => {
         if (state === "Habitants") {
@@ -14,12 +22,14 @@ document.addEventListener("DOMContentLoaded", () => {
             faune.style.transition = "3s";
             faune.style.opacity = "1";
 
-            habitants.style.scale = "0.8"
-            habitants.style.transition = "3s"
-            habitants.style.opacity = "0.38"
+            partieFaune.style.display = "";
 
-            planet.style.transform = "rotate(90deg)"
-            planet.style.transition = "3s"
+            habitants.style.scale = "0.8";
+            habitants.style.transition = "3s";
+            habitants.style.opacity = "0.38";
+
+            planet.style.transform = "rotate(90deg)";
+            planet.style.transition = "3s";
 
             Habitants.style.display = "none";
             state = "Faune";
@@ -29,12 +39,14 @@ document.addEventListener("DOMContentLoaded", () => {
             flaure.style.transition = "3s";
             flaure.style.opacity = "0.38";
 
-            habitants.style.scale = "1"
-            habitants.style.transition = "3s"
-            habitants.style.opacity = "1"
+            partieFlaure.style.display = "none";
 
-            planet.style.transform = "rotate(0deg)"
-            planet.style.transition = "3s"
+            habitants.style.scale = "1";
+            habitants.style.transition = "3s";
+            habitants.style.opacity = "1";
+
+            planet.style.transform = "rotate(0deg)";
+            planet.style.transition = "3s";
 
             Habitants.style.display = "";
             state = "Habitants"; 
@@ -49,13 +61,15 @@ document.addEventListener("DOMContentLoaded", () => {
                 flaure.style.scale = "1.2";
                 flaure.style.transition = "3s";
                 flaure.style.opacity = "1";
+
+                partieFlaure.style.display = "";
     
-                habitants.style.scale = "0.8"
-                habitants.style.transition = "3s"
-                habitants.style.opacity = "0.38"
+                habitants.style.scale = "0.8";
+                habitants.style.transition = "3s";
+                habitants.style.opacity = "0.38";
     
-                planet.style.transform = "rotate(-90deg)"
-                planet.style.transition = "3s"
+                planet.style.transform = "rotate(-90deg)";
+                planet.style.transition = "3s";
     
                 Habitants.style.display = "none";
                 state = "Flaure"; 
@@ -64,13 +78,15 @@ document.addEventListener("DOMContentLoaded", () => {
                 faune.style.scale = "1";
                 faune.style.transition = "3s";
                 faune.style.opacity = "0.38";
+
+                partieFaune.style.display = "none";
     
-                habitants.style.scale = "1"
-                habitants.style.transition = "3s"
-                habitants.style.opacity = "1"
+                habitants.style.scale = "1";
+                habitants.style.transition = "3s";
+                habitants.style.opacity = "1";
     
-                planet.style.transform = "rotate(0deg)"
-                planet.style.transition = "3s"
+                planet.style.transform = "rotate(0deg)";
+                planet.style.transition = "3s";
     
                 Habitants.style.display = "";
                 state = "Habitants"; 
